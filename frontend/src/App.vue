3305887b7790faef459cfd26d46405c1177ffa65
Swan <swan.bilek@efrei.net>
2021-06-15 10:24:23 +0200

    add fichier frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="getAllUsers">GET</button>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+      return {
+          msg: '',
+      }
+  },
+  methods: {
+    async getAllUsers() {
+      let res = await fetch('/api/users')
+      var data = await res.json();
+      console.log(data)
+      this.msg = data.mess
+      
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

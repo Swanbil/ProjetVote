308c8b0d8fd655101e8edf9efed69d938eaf002c
Swan <swan.bilek@efrei.net>
2021-06-15 10:41:13 +0200

    test modif

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="getAllUsers">GET</button>
+    <button @click="getAllUsers">TEST</button>
     {{msg}}
   </div>
 </template>

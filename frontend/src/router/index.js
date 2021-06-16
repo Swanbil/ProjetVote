58830842c6d9f849ddfea3627e372483dd840c52
Swan <swan.bilek@efrei.net>
2021-06-16 09:54:06 +0200

    ajout router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Vote from '@/components/Vote'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+        path: '/vote',
+        name: 'Vote',
+        component: Vote
+      }
+  ]
+})

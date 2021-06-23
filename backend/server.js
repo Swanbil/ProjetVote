22b316a521aa6630361388b113d0172cd40cdc3e
SachaBOUDHOU <sacha.boudhou@efrei.net>
2021-06-23 18:30:14 +0200

    ajout modifie elections

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
+const { Router } = require('express');
 const express = require('express');
 const path = require('path');
 const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
+bodyParser = require("body-parser");
+port = 3080;
+const { Client } = require ('pg')
 
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  password: 'sacha',
+  database: 'e-vote'
+})
+client.connect()
 // place holder for the data
 const users = [];
 
@@ -22,3 +31,73 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
+
+//Ajout d'élections
+app.post('/api/addElection', async(req, res) => {
+  const categorie = req.body.categorie
+  const date = req.body.date
+  const dateF = req.body.dateF
+  const description = req.body.descri
+  console.log ("desctest" +" " + categorie);
+
+  // si un champ n'est pas rempli 
+  if (categorie === '' || date === '' || dateF === '' || description=== '') {
+    res.status(400).json({ message: 'Veuillez remplir tous les champs' })
+    return
+  }
+
+  const result = await client.query({
+    text: 'SELECT idcat FROM categorieelection WHERE electype =$1',
+    values: [categorie]
+  })
+  console.log(result.rows);
+
+  await client.query({
+    text: 'INSERT INTO election(datedebut, datefin, description, idcat) values ($1,$2,$3,$4)',
+    values:[date, dateF, description, result.rows[0].idcat]
+  })
+  res.json({mess:"Election ajoutée !"});
+})
+
+//Affichage des éléection pour les modifier
+app.post('/api/affElec', async(req, res) => {
+  const categorie = req.body.categorie
+  console.log ("desctest" +" " + categorie);
+
+  const result = await client.query({
+    text: 'SELECT idcat FROM categorieelection WHERE electype =$1',
+    values: [categorie]
+  });
+  console.log(result.rows)
+  const aff=await client.query({
+      text: 'SELECT *  FROM election WHERE idcat=$1',
+      values: [result.rows[0].idcat]
+  });
+  res.json(aff.rows)
+  //si il n'ya pas d'election erreur
+}),
+
+//Modifie election
+//Affichage des éléection pour les modifier
+app.post('/api/modElec', async(req, res) => {
+  const categorie = req.body.categorie
+  const date = req.body.date
+  const dateF = req.body.dateF
+  const description = req.body.descri
+  const idelection=req.body.elections
+
+  console.log ("desctest" +" " + categorie);
+
+  const result = await client.query({
+    text: 'SELECT idcat FROM categorieelection WHERE electype =$1',
+    values: [categorie]
+  });
+  console.log(result.rows)
+
+
+  const aff=await client.query({
+      text: 'UPDATE election SET datedebut=$1, datefin=$2, description=$3 WHERE idelection =$4',
+      values: [date,dateF,description,idelection]
+  });
+  res.json(aff.rows)
+})
--- frontend/src/components/Admin.vue
+++ frontend/src/components/Admin.vue
@@ -1,38 +1,113 @@
 <template>
-<div class="PageAdmin">
-  <div class="AccueilAdmin">
+<div class="pageAdmin">
+  <div class="accueilAdmin">
     <h1>{{ title }}</h1>
     <p>Here, you can see the results or manage users</p>
-    <button type="button" @click="ShowElection()"> Gerer elections </button>
-    <button @click="ShowVote()">Gerer les votants</button>
-    <button @click="ShowNews()">Gerer les news </button>
+    <button type="button" @click="showElection()"> Gerer elections </button>
+    <button @click="showVote()">Gerer les candidats</button>
+    <button @click="showNews()">Gerer les news </button>
     <button @click="ShowStats()">Observer les statistiques des élections</button>
   </div>
 
   <div id="Election">
-    <button @click="AJouterElection()">Ajouter une nouvelle élection</button>
+    <button @click="ajouterElection()">Ajouter une nouvelle élection</button>
     <button @click="SupprimerElection()">Supprimer une elections</button>
-    <button @click="ModifierElection()">Modiier une election </button>
+    <button @click="modifierElection()">Modifier une election </button>
   </div>
   <div id="Vote">
-    <button @click="AJouterVotant()">Ajouter un nouveau votant</button>
-    <button @click="SupprimerVotant()">Supprimer un votant</button>
-    <button @click="ModifierVotant()">Modiier un votant </button>
+    <button @click="ajoutercandidat()">Ajouter un nouveau candidat</button>
+    <button @click="Supprimercandidat()">Supprimer un candidat</button>
+    <button @click="Modifiercandidat()">Modiier un candidat </button>
   </div>
   <div id="News">
     <button @click="AJouterNews()">Ajouter une actualité</button>
     <button @click="SupprimerNews()">Supprimer une actualité </button>
     <button @click="ModifierNews()">Modiier une actualité </button>
   </div>
+
+   <div id="ajoutElections">
+     <form @submit.prevent="ajElec()" method="POST">
+          <label for="statut-select" class="elec">
+            Choisir votre catégorie d'elections :
+          </label>
+
+          <select id="statut-select" v-model="categorie" class="elec">
+            <option value="" disabled selected class="elec">Catégorie</option>
+            <option value="presidentielle">Présidentielle</option>
+            <option value="municipale">Municipale</option>
+            <option value="regionale">Régionale</option>
+          </select>
+
+          <p id=datD>Selectionnez la date de début</p>
+          <input type="date" v-model="date" placeholder="Date de début" required />
+
+          <p id=datF>Selectionnez la date de fin</p>
+          <input type="date" v-model="dateF" placeholder="Date de fin" required/>
+
+          <input type="text" v-model="descri" placeholder="Entrez une descrption" required/>
+
+          <button type="submit">Valider</button>
+      </form>
+
+  </div>
+
+  <div id="modifElections">
+    <form @submit.prevent="AffichageElec()" method="POST">
+
+        <label for="statut-select" class="modifelec">
+            Choisissez votre catégorie d'elections à modifier :
+        </label>
+        <select id="statut-select" v-model="categorie" class="modifelec">
+          <option value="" disabled selected class="modifelec">Catégorie</option>
+          <option value="presidentielle">Présidentielle</option>
+          <option value="municipale">Municipale</option>
+          <option value="regionale">Régionale</option>
+        </select>       
+    </form>
+
+      <div classe ="affichageElec">
+        <button @click="affichageElec()">Ajouter</button>
+      </div>
+      <div  v-for="election in elections" v-bind:key="election.idelection">
+        {{election.idelection}}
+        {{election.datedebut}}
+        {{election.datefin}}
+        {{election.descrption}}
+        <button @click="modifieElec()">Modifier cette election</button>
+        <div id=elmod>
+          <form @submit.prevent="mdElec()" method="POST">
+
+            <p id=datD>Selectionnez la date de début</p>
+            <input type="date" v-model="date" placeholder="Date de début" required />
+
+            <p id=datF>Selectionnez la date de fin</p>
+            <input type="date" v-model="dateF" placeholder="Date de fin" required/>
+
+            <input type="text" v-model="descri" placeholder="Entrez une descrption" required/>
+
+            <button type="submit">Valider</button>
+          </form>
+        </div>
+      </div>
+  </div>  
+
+
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Admin',
   data(){
     return{
-      title:'Admin'
+      title:'Admin',
+      date:'',
+      dateF: '',
+      categorie: '',
+      descri: '',
+      affElec : '',
+      elections : [],
     }
     
   },
@@ -42,29 +117,78 @@ export default {
 
 
   methods : {
-    ShowElection(){
+    showElection(){
       document.getElementById ("Election").style.display="flex";
       document.getElementById ("Vote").style.display="none";
       document.getElementById ("News").style.display="none";
 
 
     },
-    ShowVote(){
+    showVote(){
       document.getElementById ("Vote").style.display="flex";
       document.getElementById ("Election").style.display="none";
       document.getElementById ("News").style.display="none";
 
     },
-    ShowNews(){
+    showNews(){
       document.getElementById ("Vote").style.display="none";
       document.getElementById ("Election").style.display="none";
       document.getElementById ("News").style.display="flex";
 
+    },
+    ajouterElection(){
+      document.getElementById ("ajoutElections").style.display="flex";
+      document.getElementById ("modifElections").style.display="none";
+
 
     },
-}
-}
+    modifierElection(){
+      document.getElementById ("modifElections").style.display="flex";
+      document.getElementById ("ajoutElections").style.display="none";
 
+
+    },
+
+    //ajout election
+    async ajElec(){
+      console.log (this.categorie),
+      await axios.post('/api/addElection', {
+        date: this.date,
+        dateF: this.dateF,
+        categorie: this.categorie,
+        descri: this.descri
+      });
+      /*this.$router.push("/");*/
+    },
+
+    async affichageElec() {
+      console.log(this.categorie)
+      const affElec= await axios.post ('/api/affElec',{
+      categorie: this.categorie,
+      });
+      console.log(affElec.data)
+      this.elections = affElec.data
+
+    },
+
+    modifieElec() {
+      document.getElementById ("elmod").style.display="flex";
+
+    },
+    
+   //modif election
+    async mdElec(){
+      console.log (this.categorie),
+      await axios.post('/api/modElec', {
+        date: this.date,
+        dateF: this.dateF,
+        categorie: this.categorie,
+        descri: this.descri,
+        elections : this.elections
+      });
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -85,6 +209,15 @@ h1{
   display : none;
 }
 #News{
+  display : none;
+}
+#ajoutElections{
+  display : none;
+}
+#modifElections{
+  display: none;
+}
+#elmod{
   display : none;
 }
 
